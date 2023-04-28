1f73b774369287843fcfae526a8585db4f7e40bc
jetty <dlwnsdud980727@gmail.com>
2023-04-28 16:24:10 +0900

    First commit

---
+++ src/test/java/GraduateProject/HongIkGP/HongIkGpApplicationTests.java
@@ -0,0 +1,13 @@
+package GraduateProject.HongIkGP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongIkGpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
